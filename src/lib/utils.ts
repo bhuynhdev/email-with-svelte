62a867577c910c62083405c29e04fa489c010314
bhuynhdev <baoh.inbox@gmail.com>
2025-10-18 21:03:53 -0400

    `styleToString` to ignore undefined values

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,17 +1,14 @@
-import type { StandardProperties } from "csstype";
+import type { StandardProperties } from 'csstype';
 
 // Credit: https://stackoverflow.com/a/61410824
+// Modified to ignore `undefined/null` keys and use Array.join for better memory efficiency
 export const styleToString = (style: StandardProperties) => {
-	return Object.keys(style).reduce(
-		(acc, key) =>
-			acc +
-			key
-				.split(/(?=[A-Z])/)
-				.join('-')
-				.toLowerCase() +
-			':' +
-			style[key as keyof typeof style] +
-			';',
-		''
-	);
+	return Object.entries(style)
+		.filter(([_, value]) => value != null) // Use `!=` (instead of !==) to filter out both null and undefined
+		.map(([key, value]) => {
+			const camelCaseKey = key.replace(/([A-Z])/g, '-$1').toLowerCase();
+			const cssRuleString = `${camelCaseKey}:${value};`;
+			return cssRuleString;
+		})
+		.join('');
 };
