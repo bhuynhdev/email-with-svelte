b9ba3d58ef75708dd2c7dde15a96a97822b26483
bhuynhdev <baoh.inbox@gmail.com>
2025-10-19 17:10:47 -0400

    Add Head component

---
+++ src/lib/components/Head.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { SvelteHTMLElements } from 'svelte/elements';
+	type HeadProps = SvelteHTMLElements['head'];
+
+	const { children, ...props }: HeadProps = $props();
+</script>
+
+<head {...props}>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<meta name="x-apple-disable-message-reformatting" />
+	{@render children?.()}
+</head>
