034f2a039ee373c53c5b30e0d2d274236849af36
bhuynhdev <baoh.inbox@gmail.com>
2025-10-18 21:33:20 -0400

    Add `Link` component

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { styleToString } from '$lib/utils/style-to-string.js';
+	import type { StandardProperties } from 'csstype';
+	import type { HTMLAnchorAttributes } from 'svelte/elements';
+
+	type LinkProps = Omit<HTMLAnchorAttributes, 'style' | 'class'> & {
+		style?: StandardProperties;
+	};
+
+	let { children, target = '_blank', style, ...props }: LinkProps = $props();
+</script>
+
+<a
+	{...props}
+	style={styleToString({
+		color: '#067df7',
+		textDecorationLine: 'none',
+		...style
+	})}
+	{target}
+>
+	{@render children?.()}
+</a>
