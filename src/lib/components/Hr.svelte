1d5c43b9b92b8ab4785bc6082b44e3c2c913d83a
bhuynhdev <baoh.inbox@gmail.com>
2025-10-19 20:58:04 -0400

    Add Hr component

---
+++ src/lib/components/Hr.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { styleToString } from '$lib/utils/style-to-string.js';
+	import type { StandardProperties } from 'csstype';
+	import type { SvelteHTMLElements } from 'svelte/elements';
+
+	type HrProps = Omit<SvelteHTMLElements['hr'], 'style' | 'class'> & {
+		style?: StandardProperties;
+	};
+
+	let { children, style = {}, ...props }: HrProps = $props();
+</script>
+
+<hr
+	{...props}
+	style={styleToString({ width: '100%', border: 'none', borderTop: '1px solid #eaeaea', ...style })}
+/>
