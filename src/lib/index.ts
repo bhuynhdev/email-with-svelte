0300e7c70b30f81915410c6260f60b590d6c0a57
bhuynhdev <baoh.inbox@gmail.com>
2025-10-19 21:14:47 -0400

    Export components

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,15 @@
 import Body from './components/Body.svelte';
+import Button from './components/Button.svelte';
 import Column from './components/Column.svelte';
 import Container from './components/Container.svelte';
+import Font from './components/Font.svelte';
+import Head from './components/Head.svelte';
+import Hr from './components/Hr.svelte';
 import Html from './components/Html.svelte';
+import Img from './components/Img.svelte';
+import Link from './components/Link.svelte';
 import Row from './components/Row.svelte';
+import Section from './components/Section.svelte';
 import Text from './components/Text.svelte';
 
-export { Body, Column, Container, Html, Row, Text };
+export { Body, Button, Column, Container, Font, Head, Hr, Html, Img, Link, Row, Section, Text };
