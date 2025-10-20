9ef2ad2ebbb9ea7aed1dc09b411029fc9d90dceb
bhuynhdev <baoh.inbox@gmail.com>
2025-10-19 21:14:04 -0400

    Add Section component

---
+++ src/lib/components/Section.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { styleToString } from '$lib/utils/style-to-string.js';
+	import type { StandardProperties } from 'csstype';
+	import type { HTMLTableAttributes } from 'svelte/elements';
+
+	type SectionProps = Omit<HTMLTableAttributes, 'style' | 'class'> & {
+		style?: StandardProperties;
+	};
+
+	let { children, style = {}, ...props }: SectionProps = $props();
+</script>
+
+<table
+	align="center"
+	width="100%"
+	border={0}
+	cellPadding="0"
+	cellSpacing="0"
+	role="presentation"
+	{...props}
+	style={styleToString(style)}
+>
+	<tbody>
+		<tr>
+			<td>{@render children?.()}</td>
+		</tr>
+	</tbody>
+</table>
