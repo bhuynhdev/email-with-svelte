fb41dc8f21be6dc8daa3cd7c7bc77c633fd34de2
bhuynhdev <baoh.inbox@gmail.com>
2025-10-19 21:09:09 -0400

    Add Img component

---
+++ src/lib/components/Img.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { styleToString } from '$lib/utils/style-to-string.js';
+	import type { StandardProperties } from 'csstype';
+	import type { HTMLImgAttributes } from 'svelte/elements';
+
+	type ImgProps = Omit<HTMLImgAttributes, 'style' | 'class'> & {
+		style?: StandardProperties;
+	};
+
+	// Capture any other attributes (like loading, decoding, etc.)
+	const { alt, height, width, src, style = {}, ...props }: ImgProps = $props();
+</script>
+
+<img
+	{...props}
+	{alt}
+	{src}
+	{width}
+	{height}
+	style={styleToString({
+		display: 'block',
+		outline: 'none',
+		border: 'none',
+		textDecoration: 'none',
+		...style
+	})}
+/>
